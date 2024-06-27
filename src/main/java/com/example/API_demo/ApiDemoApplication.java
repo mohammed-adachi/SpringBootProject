2daca46f8080cb19b21f622e8602a4a189532c66
Mohamed <mohame@gmail.com>
2024-06-27 18:52:51 +0100

    first commit

---
+++ src/main/java/com/example/API_demo/ApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.API_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/API_demo/hellocontroller.java
@@ -0,0 +1,10 @@
+package com.example.API_demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class hellocontroller {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "bonjour tout le monde !";
+    }
+}
